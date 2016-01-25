cdc6db3658d0d9351540ca0860173d90cf6c7f25
Hector Ramos <hector.ramos@ninthdesign.com>
2016-01-25 15:53:31 +0800

    Add Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,55 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function(mix) { 
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets'; 
+    mix.sass('app.scss') 
+        .copy(bootstrapPath + '/fonts', 'public/fonts') 
+        .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js'); 
 });
+
+
+// // Include gulp
+// var gulp = require('gulp'); 
+
+// // Include Our Plugins
+// var jshint = require('gulp-jshint');
+// var sass = require('gulp-sass');
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
+
+// // Lint Task
+// gulp.task('lint', function() {
+//     return gulp.src('js/*.js')
+//         .pipe(jshint())
+//         .pipe(jshint.reporter('default'));
+// });
+
+// // Compile Our Sass
+// gulp.task('sass', function() {
+//     return gulp.src('scss/*.scss')
+//         .pipe(sass())
+//         .pipe(gulp.dest('css'));
+// });
+
+// // Concatenate & Minify JS
+// gulp.task('scripts', function() {
+//     return gulp.src('js/*.js')
+//         .pipe(concat('all.js'))
+//         .pipe(gulp.dest('dist'))
+//         .pipe(rename('all.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist'));
+// });
+
+// // Watch Files For Changes
+// gulp.task('watch', function() {
+//     gulp.watch('js/*.js', ['lint', 'scripts']);
+//     gulp.watch('scss/*.scss', ['sass']);
+// });
+
+// // Default Task
+// gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+
+
